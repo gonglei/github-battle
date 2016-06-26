f115440db267d7934fdc03ce29f9d3ee0be2dfbd
Lei Gong <leigong@ca.ibm.com>
2016-06-26 07:37:43 -0400

    Add a wrapper component for details;

--- app/components/ConfirmBattle.js
+++ app/components/ConfirmBattle.js
@@ -3,6 +3,7 @@ var styles = require('../styles');
 var Link = require('react-router').Link;
 var PropTypes = React.PropTypes;
 var UserDetails = require('./UserDetails');
+var UserDetailsWrapper = require('./UserDetailsWrapper');
 
 function puke(obj) {
     return <pre>{JSON.stringify(obj, null, ' ')}</pre>;
@@ -15,14 +16,12 @@ function ConfirmBattle(props) {
         : (<div className='jumbotron col-sm-12 text-center' style={styles.transparentBg}>
             <h1>Confirm Players</h1>
             <div className='col-sm-8 col-sm-offset-2'>
-                <div className='col-sm-6'>
-                    <p className='lead'>Player 1</p>
+                <UserDetailsWrapper header="Player One">
                     <UserDetails info={props.playersInfo[0]} />
-                </div>
-                <div className='col-sm-6'>
-                    <p className='lead'>Player 2</p>
+                </UserDetailsWrapper>
+                <UserDetailsWrapper header="Player Two">
                     <UserDetails info={props.playersInfo[1]} />
-                </div>
+                </UserDetailsWrapper>
             </div>
             <div className='col-sm-8 col-sm-offset-2'>
                 <div className='col-sm-12' style={styles.space}>
